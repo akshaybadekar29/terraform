d18cd5adcdafe4ea4e2774b07853955b3a971d91
EC2 Default User <ec2-user@webserver.mydomain.com>
2019-10-04 22:45:43 +0000

    removed prefix id

--- ec2.tf
+++ ec2.tf
@@ -34,7 +34,6 @@ resource "aws_security_group" "Web_Security_Group" {
     to_port         = 0
     protocol        = "-1"
     cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = ["pl-12c4e678"]
   }
   
 }
