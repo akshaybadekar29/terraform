a3a08fd97211aaecc3b29c6cdb828f08c045d3da
akshaybaedkar29 <akshaybaedkar29@gmail.com>
2019-10-05 04:13:28 +0530

    added egress rule

--- ec2.tf
+++ ec2.tf
@@ -29,5 +29,12 @@ resource "aws_security_group" "Web_Security_Group" {
         protocol ="tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = ["pl-12c4e678"]
+  }
   
 }
