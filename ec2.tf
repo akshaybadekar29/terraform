b48d645df7a8cd0b41a01b79be51df607d7641c0
akshaybaedkar29 <akshaybaedkar29@gmail.com>
2019-10-04 23:13:06 +0530

    changes in userdata

--- ec2.tf
+++ ec2.tf
@@ -6,12 +6,15 @@ resource "aws_instance" "webserver" {
 
     ami = "ami-0b69ea66ff7391e80"
     instance_type ="t2.micro"
-    tags = {
-        Name = "webserver"
-    }
     user_data = <<-EOF
                 #!/bin/bash
                 echo "hello world"
+                yum update -y
+                yum install -y httpd
+                systemctl start httpd
                 EOF
-  
+    tags = {
+        Name = "webserver"
+    }
+
 }
