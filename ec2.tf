69ae0978e7db58b7ad453315dfcbc99c4fb672ec
akshaybaedkar29 <akshaybaedkar29@gmail.com>
2019-10-05 04:55:40 +0530

    chnages

--- ec2.tf
+++ ec2.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "webserver" {
     ami = "ami-0b69ea66ff7391e80"
     instance_type ="t2.micro"
     vpc_security_group_ids =  [ aws_security_group.Web_Security_Group.id ]
-    user_data = << EOF
+    user_data = <<-EOF
                 #! /usr/bin/bash
                 echo "hello world"
                 yum update -y
