f963914eeeedc84385e77f739314fa39dd1a2ce4
akshaybaedkar29 <akshaybaedkar29@gmail.com>
2019-10-05 06:35:06 +0530

    added auto scalling group ,launch configuration

--- ec2.tf
+++ ec2.tf
@@ -6,11 +6,37 @@ data "template_file" "userdata" {
   template = "${file("template/userdata.tpl")}"
 }
 
+data "aws_vpc" "default" {
+    default =true
+  
+}
+
+data "aws_subnet_ids" "default" {
+
+    vpc_id = data.aws_vpc.default.id
+  
+}
+
+
 variable "http_port" {
 
-    description = "and exaple of a number veriable in terraform"
+    description = "http port"
     type = number
     default = 80
+  
+}
+
+variable "asg_min_size" {
+    description = "number of ec2 minimum instance auto scalling group"
+    type =number
+    default=2
+  
+}
+
+variable "asg_max_size" {
+    description = "number of ec2 maximum instance auto scalling group"
+    type =number
+    default=3
   
 }
 
@@ -43,4 +69,29 @@ resource "aws_security_group" "Web_Security_Group" {
     cidr_blocks     = ["0.0.0.0/0"]
   }
   
+}
+
+resource "aws_launch_configuration" "web_server_launch_config" {
+   image_id = "ami-0b69ea66ff7391e80"
+   instance_type = "t2.micro"
+   security_groups = [ aws_security_group.Web_Security_Group.id ]
+   user_data = "${data.template_file.userdata.rendered}"
+}
+
+resource "aws_autoscaling_group" "web_server_autoscalling_group" {
+
+  launch_configuration = aws_launch_configuration.web_server_launch_config.name
+  vpc_zone_identifier =  data.aws_vpc.default.id
+  min_size = var.asg_min_size
+  max_size = var.asg_max_size
+  tag{
+      key = "Name"
+      value = "ASG terraform"
+      propagate_at_launch = true
+
+  }
+  lifecycle{
+  
+  create_before_destroy=true
+  }
 }
