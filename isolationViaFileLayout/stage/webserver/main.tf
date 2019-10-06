e4526268cf762309f2477259602969783a9cb1dc
akshaybaedkar29 <akshaybaedkar29@gmail.com>
2019-10-06 06:24:57 +0530

    added  prod stage envrioment to isolate using le layout

---
+++ isolationViaFileLayout/stage/webserver/main.tf
@@ -0,0 +1,46 @@
+provider "aws"{
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "terraform_state_bucket" {
+    bucket = "terraform-state-aws-bucket"
+    lifecycle{
+        prevent_destroy = true 
+
+    }
+    versioning {
+        enabled = true
+    }
+
+    server_side_encryption_configuration{
+        rule{
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+  
+}
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = "terraform-state"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+  
+}
+
+terraform {
+    backend "s3"
+    {
+        bucket = "terraform-state-aws-bucket"
+        key = "global/stage/webserver/terraform.tfstate"
+        region = "us-east-1"
+        dynamodb_table = "terraform-state"
+        encrypt = true
+    }
+}
+
